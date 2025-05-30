d8da8adb864a0c5e88180a0aa8a0e691f07a251e
shivensh <shivenshmeena0752@gmail.com>
2025-05-30 13:31:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "2695501"
 api_hash = "9a544068ea55f54ca7725245d0e8f0f9"
-bot_token = "7876227653:AAEl1CpmAk5zYK4qEaYwv3vr1CyYW4cyLUY"
+bot_token = "7920462477:AAHE7HZFx2o3TwmXZvZte8cLzmO8kLQ-ZiY"
 auth_users = [8049118708]
